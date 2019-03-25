8c08c470314da75de87681aa7ab0cc5661c3e559
Matt <mlichti@gmail.com>
2019-03-26 01:14:28 +0800

    try to download both dl models

--- app/server.py
+++ app/server.py
@@ -124,10 +124,8 @@ pclasses = ['Discmania C-line',
  'Westside Tournament',
  'Westside Vip']
 
-export_file_url = 'https://www.dropbox.com/s/8qtxued68q92may/35plastics.pkl?dl=1'
-export_file_name = '35plastics.pkl'
-classes = pclasses
-
+plastics_export_file_url = 'https://www.dropbox.com/s/8qtxued68q92may/35plastics.pkl?dl=1'
+plastics_export_file_name = '35plastics.pkl'
 
 path = Path(__file__).parent
 
@@ -144,6 +142,7 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
+    await download_file(plastics_export_file_url, path/plastics_export_file_name)
     try:
         learn = load_learner(path, export_file_name)
         return learn
